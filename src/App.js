4b0fe8657b6384546d107ae62902aacc92c5c905
jiwoorld <bncmdbev@gmail.com>
2023-09-13 00:07:30 +0900

    💄  StartPage 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./store"; 
+import { store, persistor } from "./store";
 import TodoList from "./TodoList";
+import StartPage from "./StartPage";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <div className="App">
-          <TodoList />
-        </div>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<StartPage />} />
+              <Route path="/todo" element={<TodoList />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   );
