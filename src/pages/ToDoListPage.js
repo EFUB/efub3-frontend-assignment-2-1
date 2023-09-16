c5c2f306ea9873b1403199730dd97ef77582cd61
Yerineee <yerin1103@ewhain.net>
2023-09-16 17:29:24 +0900

    :sparkles: 색상 선택 버튼 추가

--- src/pages/ToDoListPage.js
+++ src/pages/ToDoListPage.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ToDoList from "../components/ToDoListPage/ToDoList";
 import ToDoInput from "../components/ToDoListPage/ToDoInput";
 
 const ToDoListPage = () => {
+  const nav = useNavigate();
   const color = useSelector((state) => state.color); // store에 저장된 color 가져오기
 
   return (
@@ -11,6 +13,9 @@ const ToDoListPage = () => {
       <Title>To Do List</Title>
       <ToDoList />
       <ToDoInput />
+      <NavBtn color={color.btnColor} onClick={() => nav("/")}>
+        색상 다시 선택하기
+      </NavBtn>
     </Wrapper>
   );
 };
@@ -19,7 +24,7 @@ export default ToDoListPage;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -32,4 +37,20 @@ const Title = styled.div`
   font-family: "MYYeongnamnu";
   font-size: 70px;
   text-align: center;
+`;
+
+const NavBtn = styled.button`
+  all: unset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 170px;
+  height: 50px;
+  margin-top: 50px;
+
+  font-family: "MYYeongnamnu";
+  font-size: 20px;
+  background-color: ${(props) => props.color};
+  border-radius: 40px 80px / 80px 40px;
+  cursor: pointer;
 `;
