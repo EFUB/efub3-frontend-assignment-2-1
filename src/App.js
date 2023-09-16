b7ab561017035bc676d9b4b5d1c9304e587c5321
Yerineee <yerin1103@ewhain.net>
2023-09-16 17:05:17 +0900

    :bug: 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import MainPage from "./pages/MainPage";
 import ToDoListPage from "./pages/ToDoListPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ToDoListPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/to-do-list" element={<ToDoListPage />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
