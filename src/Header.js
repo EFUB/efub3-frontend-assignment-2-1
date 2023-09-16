1528ab4b90452feb93127f336dbfe28bd80e5e74
wowalswjd <alswjd5491@naver.com>
2023-09-17 04:11:17 +0900

    :sparkles: Header에 장바구니 상품 개수 추가

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,24 @@
 import styled from "styled-components";
 import logo from "./assets/eight_seconds_logo.png";
-import heart from "./assets/ic_heart.png";
 import cart from "./assets/ic_cart.png";
 import { useNavigate } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const Header = () => {
   const navigate = useNavigate();
+
+  // 장바구니 목록 불러오기
+  const cartList = useSelector((state) => state.cart);
+
+  const [curCartList, setCurCartList] = useState(cartList);
+
+  // 장바구니 목록이 바뀔 때마다 갱신하기
+  useEffect(() => {
+    setCurCartList(cartList);
+  }, [cartList]);
+
   return (
     <HeaderWrapper>
       <Logo
@@ -16,13 +29,16 @@ const Header = () => {
         }}
       />
 
-      <CartIcon
-        src={cart}
-        alt="장바구니"
-        onClick={() => {
-          navigate("/cart");
-        }}
-      />
+      <div style={{ position: "relative" }}>
+        <CartIcon
+          src={cart}
+          alt="장바구니"
+          onClick={() => {
+            navigate("/cart");
+          }}
+        />
+        <CartQuantity>{curCartList.length}</CartQuantity>
+      </div>
     </HeaderWrapper>
   );
 };
@@ -46,17 +62,21 @@ const Logo = styled.img`
   width: 13%;
 `;
 
-const HeartIcon = styled.img`
-  width: 38px;
-  margin-right: 15px;
-  @media screen and (max-width: 600px) {
-    width: 30px;
-  }
-`;
-
 const CartIcon = styled.img`
   width: 44px;
   @media screen and (max-width: 600px) {
     width: 33px;
   }
+`;
+
+const CartQuantity = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: #e91313;
+  color: white;
+
+  font-size: 14px;
+  padding: 1px 8px;
+  border-radius: 10px;
 `;
