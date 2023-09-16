561554afd46ea3ef215c9f04501620ad8f2d4c14
Yerineee <yerin1103@ewhain.net>
2023-09-16 17:06:25 +0900

    :art: 주석 추가

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -4,15 +4,18 @@ const toDoSlice = createSlice({
   name: "toDoSlice",
   initialState: [],
   reducers: {
+    // 할 일 추가
     add: (state, action) => {
       state.push({
         text: action.payload,
         isCompleted: false,
       });
     },
+    // 할 일 삭제
     remove: (state, action) => {
       return state.filter((item, index) => index !== action.payload);
     },
+    // 할 일 완료
     complete: (state, action) => {
       return state.map((item, index) =>
         index === action.payload
